28d106e741bc83965609b16d140fdbd1e86c2d13
jwlee1108 <jw.lee@nhn.com>
2023-11-28 22:01:41 +0900

    exercise 5

--- src/providers/ToastsProvider/ToastsProvider.js
+++ src/providers/ToastsProvider/ToastsProvider.js
@@ -4,6 +4,19 @@ export const ToastsContext = React.createContext();
 function ToastsProvider({ children }) {
   const [toasts, setToasts] = React.useState([]);
 
+  React.useEffect(() => {
+    function handleKeydown(ev) {
+      if (ev.code === "Escape") {
+        setToasts([]);
+      }
+    }
+
+    window.addEventListener('keydown', handleKeydown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeydown);
+    }
+  }, [])
 
   return (
     <ToastsContext.Provider value={{ toasts, setToasts }}>
